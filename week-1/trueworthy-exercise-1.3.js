6315de14be017a43f2cd431de283893e943fc0b0
Lea Trueworthy <ltrueworthy@my365.bellevue.edu>
2019-02-20 14:33:50 -0800

    assignment 1.3

---
+++ week-1/trueworthy-exercise-1.3.js
@@ -0,0 +1,47 @@
+var header = require('/Users/leatrueworthy/bu-webdev/web-330/trueworthy-header');
+console.log(header.display("Lea", "Trueworthy", "Exercise 1.3"));
+
+/*
+ Expected output:
+
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ -- DISPLAYING CELL PHONE DETAILS --
+ Manufacturer: <manufacturer>
+ Model: <model>
+ Color: <color>
+ Price: <price>
+
+
+*/
+
+// start program
+
+function Cellphone(manufacturer, model, color, price)
+{
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.color = color;
+    this.price = price;
+
+
+//get
+this.getPrice = function()
+{
+    return this.price;
+}
+this.getModel = function(){
+    return this.model;
+}
+
+this.getDetails = function()
+    {
+        return "\nManufacturer:" + this.manufacturer + "\nModel:" + this.model + "\nColor: " + this.color + "\nPrice: " + "$" + this.getPrice();
+    }
+}
+
+var cellphone = new Cellphone("Samsung", "Galaxy 9", "Lilac Purple", "699.99");
+console.log(cellphone.getDetails());
+// end program
